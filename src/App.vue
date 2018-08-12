174c508bceee92f5af5c43380dbf8e1a9a266aaa
Luciano Bueno <glamero@gmail.com>
2018-08-12 20:39:56 -0300

    bump

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="q-app">
+    <!-- <router-view :EventBus="EventBus" :class="($q.loading.active) ? 'invisible' : 'visible'"  /> -->
+    <router-view :EventBus="EventBus"/>
+  </div>
+</template>
+
+<script>
+/**
+* https://alligator.io/vuejs/global-event-bus/
+* vue events as message bus
+*/
+import Vue from 'vue'
+export const EventBus = new Vue();
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      EventBus : EventBus
+    }
+  },
+
+  
+}
+</script>
+
+<style>
+</style>
---
+++ src/layouts/admin-lte.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="wrapper">
+
+  <admin-lte-header/>
+  <!-- Left side column. contains the logo and sidebar -->
+  <admin-lte-left-sidebar/>
+
+  <!-- Content Wrapper. Contains page content -->
+    <router-view :EventBus="EventBus"/>
+
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <admin-lte-footer/>
+
+  <!-- Control Sidebar -->
+  <admin-lte-control-sidebar/>
+</div>
+<!-- ./wrapper -->
+
+
+</template>
+
+<script>
+// import { openURL } from 'quasar'
+
+import AdminLteHeader from './admin-lte/header'
+import AdminLteLeftSidebar from './admin-lte/leftSidebar'
+import AdminLteFooter from './admin-lte/footer'
+import AdminLteControlSidebar from './admin-lte/controlSidebar'
+
+export default {
+  name: 'admin-lte',
+
+  components: {
+    AdminLteHeader,
+    AdminLteLeftSidebar,
+    AdminLteFooter,
+    AdminLteControlSidebar
+  },
+  props: {
+    EventBus: {
+      type: [Object],
+       default: () => ({})
+    },
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    // openURL
+  },
+  created: function(){
+    // Resolve conflict in jQuery UI tooltip with Bootstrap tooltip
+    jQuery.widget.bridge('uibutton', jQuery.ui.button)
+  }
+}
+
+import 'admin-lte/dist/js/adminlte.min.js'
+</script>
+
+<style>
+</style>
