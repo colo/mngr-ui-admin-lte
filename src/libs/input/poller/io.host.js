c93748c50096cf304f01612f5cbc71dae9b7fea6
Luciano Bueno <glamero@gmail.com>
2018-09-10 17:49:49 -0300

    bump

--- src/libs/input/poller/io.host.js
+++ src/libs/input/poller/io.host.js
@@ -12,9 +12,73 @@ export default new Class({
 
     path: '/os',
 
+    status: undefined,
+
     stat_host: null,
 
   	requests : {
+      range: [
+    		{
+					sort_by_path: function(req, next, app){
+            console.log('SORT_BY_PATH RANGE', app.options.paths)
+
+            if(app.options.stat_host && this.status == 'ok'){
+              // let start_key = (app.options.path_start_key != null) ? app.options.path_start_key: app.options.path_key
+              // let end_key = (app.options.path_end_key != null ) ? app.options.path_end_key : app.options.path_key
+
+
+              // let CHUNK = 60000
+              let end = (req.opt.range.end != null) ?  req.opt.range.end : Date.now()
+              // let start = ((end - CHUNK) < req.opt.range.start) ? req.opt.range.start : end - CHUNK
+              let start = req.opt.range.start
+
+              // do {
+
+                Array.each(app.options.paths, function(path){
+                  if(!app.options.paths_blacklist || app.options.paths_blacklist.test( path ) == false){
+
+                    // //console.log('couchdb.os range', path)
+
+                      // next(
+                      // app.view({
+          						// 	uri: app.options.db,
+                      //   args: [
+                      //     'sort',
+                      //     'by_path',
+                      //     {
+            					// 			// startkey: [start_key, app.options.stat_host, "periodical", req.opt.range.start],
+            					// 			// endkey: [end_key, app.options.stat_host, "periodical",req.opt.range.end],
+                      //       // startkey: [path, app.options.stat_host, "periodical", req.opt.range.start],
+                      //       startkey: [path, app.options.stat_host, "periodical", start],
+            					// 			endkey: [path, app.options.stat_host, "periodical",end],
+                      //
+            					// 			inclusive_end: true,
+            					// 			include_docs: true
+            					// 		}
+                      //   ]
+          						// })
+                      this.io.emit('range', {
+                        host: app.options.stat_host,
+                        path: path,
+                        range: req.opt.range
+          						})
+                  }
+
+                }.bind(app))
+              //
+              //   start -= CHUNK
+              //   end -= CHUNK
+              // }
+              // while(start > req.opt.range.start)
+
+            }
+
+
+
+					}
+				},
+
+			],
       once: [
 			],
 			periodical: [
@@ -26,6 +90,12 @@ export default new Class({
 			// middlewares: [], //namespace.use(fn)
 			// rooms: ['root'], //atomatically join connected sockets to this rooms
 			routes: {
+        'host': [{
+					// path: ':param',
+					// once: true, //socket.once
+					callbacks: ['host'],
+					// middlewares: [], //socket.use(fn)
+				}],
 				'os': [{
 					// path: ':param',
 					// once: true, //socket.once
@@ -42,9 +112,20 @@ export default new Class({
 
 
   },
-
+  host: function(socket, next){
+    let {host, status} = arguments[2]
+    console.log('IO.HOST host', host, status)
+    this.status = status
+    //
+    // if(status == 'ok')
+    //   this.io.emit('range', )
+  },
   os: function(socket, next){
-    this.fireEvent('onPeriodicalDoc', [arguments[2], {type: 'periodical', input_type: this, app: null}]);
+    let {type, doc} = arguments[2]
+    console.log('IO.HOST os', arguments[2])
+
+    this.fireEvent((type == 'range') ? 'onRangeDoc' : 'onPeriodicalDoc', [arguments[2], {type: type, input_type: this, app: null}]);
+
 		// console.log('app_doc...', socket, arguments[2])
 		// arguments[1]()
 		// this.io.to('root').emit('response', 'a new user has joined the room saying '+arguments[2]);
